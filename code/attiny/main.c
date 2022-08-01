844f2d26e70e985b9b92a4f20edfd571e0a6260f
kennedn <kennedn@msn.com>
2022-08-01 16:47:06 +0100

    attiny code cleanup

--- code/attiny/main.c
+++ code/attiny/main.c
@@ -1,18 +1,23 @@
-#define F_CPU 16000000UL
-#define TIMER_TARGET 92 // 92 * 16.32ms = 1.5s
+#define F_CPU 16000000UL    // 16Mhz
+#define TIMER_TARGET 92     // 92 * 16.32ms = 1.5s
 #define IN_PIN_1 PB1
 #define IN_PIN_2 PB2
 #define OUT_PIN PB3
 
+#ifndef __AVR_ATtiny45__ 
+#define __AVR_ATtiny45__ 
+#endif
+
 #include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
 
-volatile int timer_count=0;
+volatile unsigned char timer_count=0;
 
-// Timer0 interrupt vector, called every 16.32ms -  1 / (F_CPU / 1024) * 255
+// Timer0 interrupt vector, called when the 8 bit counter TCNT0 overflows (every 16.32ms | 1 / (F_CPU / 1024) * 255)
 // Sets OUT_PIN after TIMER_TARGET is reached
-ISR (TIMER0_OVF_vect) { 
+ISR(TIMER0_OVF_vect) { 
   if (timer_count == TIMER_TARGET) { 
     timer_count = 0; 
     TIMSK &= ~(_BV(TOIE0)); // Disable timer0 interrupt
@@ -22,11 +27,11 @@ ISR (TIMER0_OVF_vect) {
   else {timer_count++;}
 }
 
-// Pin-change interrupt vector, called when either IN_PIN is pressed
-// Starts Timer0 if both IN_PINs are HIGH, else clears timer and resets OUT_PIN
+// Pin-change interrupt vector, called on rising and falling edge of IN_PINs (pressed or depressed)
+// Starts Timer0 if both IN_PINs are LOW, else clears timer and resets OUT_PIN
 ISR(PCINT0_vect) {
   // button_state = 0 if in_pins both pressed, else 1
-  uint8_t button_state = (PINB & (_BV(IN_PIN_1) | _BV(IN_PIN_2))) == 0;
+  unsigned char button_state = (PINB & (_BV(IN_PIN_1) | _BV(IN_PIN_2))) == 0;
   if (button_state) {
     TCNT0 = 0; // reset counter to 0
     TIMSK |= _BV(TOIE0); // enable timer0 interrupt
@@ -38,17 +43,19 @@ ISR(PCINT0_vect) {
 }
 
 int main(void) {
+  // Configure input / output pins
   DDRB |= ~(_BV(IN_PIN_1) | _BV(IN_PIN_2)); // Set button pins as input
   DDRB |= _BV(OUT_PIN); // Set LED pin as output
   PORTB |= _BV(OUT_PIN); // Set LED HIGH to start
 
-  sei(); // Enable interrupts
+  // Configure interrupts
   GIMSK |= _BV(PCIE); // Enable pin-change interrupt
   PCMSK |= _BV(PCINT1) | _BV(PCINT2); // Set the pin-change interrupt pins
-
-  TCCR0A = 0;   // Normal mode
+  TCCR0A = 0;   // Enable timer interrupt in normal mode
   TCCR0B = 0;
   TCCR0B |= _BV(CS00) | _BV(CS02);   // Prescale timer (Divide clock) by 1024
+
+  sei();  // Enable interrupts
 
   while(1){}
 
